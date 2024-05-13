061d7f9c747328fafaf16b10a1b166868705daff
emiliiakazakova <kazakova.emiliia@chnu.edu.ua>
2024-05-13 20:39:53 +0300

    add directors controller

---
+++ src/API/Controllers/DirectorsController.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DAL;
+using Domain.Entities;
+
+namespace API.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{apiVersion:apiVersion}/[controller]")]
+    public class DirectorsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DirectorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        // GET: api/Directors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Director>> GetDirector(int id)
+        {
+            var director = await _context.Directors.FindAsync(id);
+
+            if (director == null)
+            {
+                return NotFound();
+            }
+
+            return director;
+        }
+
+        // PUT: api/Directors/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDirector(int id, Director director)
+        {
+            if (id != director.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(director).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DirectorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Directors
+        [HttpPost]
+        public async Task<ActionResult<Director>> PostDirector(Director director)
+        {
+            _context.Directors.Add(director);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDirector", new { id = director.Id }, director);
+        }
+
+        // DELETE: api/Directors/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDirector(int id)
+        {
+            var director = await _context.Directors.FindAsync(id);
+            if (director == null)
+            {
+                return NotFound();
+            }
+
+            _context.Directors.Remove(director);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DirectorExists(int id)
+        {
+            return _context.Directors.Any(e => e.Id == id);
+        }
+    }
+}
